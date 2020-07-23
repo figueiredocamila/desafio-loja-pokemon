c3d5a2c3cf099366740289c361b702387fc6a85d
figueiredocamila <camiladiasfigueiredo@gmail.com>
2020-07-23 20:35:30 -0300

    finished button atom

---
+++ src/components/atoms/button/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="onClick" class="button">
+    <!-- <i><img src="@/assets/backpack.svg" /></i> -->
+    <span>{{text || 'botão'}}</span>
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: ['text', 'color', 'onClick'],
+};
+</script>
+<style>
+  @import './style.scss';
+</style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Button :text="'camila'" :onClick="clicado"/>
+  </div>
+</template>
+
+<script>
+import Button from '@/components/atoms/button/Button.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Button,
+  },
+  methods: {
+    clicado() {
+      console.log('clicado');
+    },
+  },
+};
+</script>
