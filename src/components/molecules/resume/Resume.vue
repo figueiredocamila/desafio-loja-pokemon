673a14d6fa50b2358e5fa3ada4d09253cd43e1e1
figueiredocamila <camiladiasfigueiredo@gmail.com>
2020-07-26 22:35:21 -0300

    iconbutton test and resume test

--- src/components/molecules/resume/Resume.vue
+++ src/components/molecules/resume/Resume.vue
@@ -6,6 +6,7 @@
 </template>
 <script>
 export default {
+  nome: 'Resume',
   props: ['value'],
 };
 </script>
---
+++ tests/unit/Resume.spec.js
@@ -0,0 +1,18 @@
+import { shallowMount } from '@vue/test-utils';
+import Resume from '@/components/molecules/resume/Resume.vue';
+
+const wrapper = shallowMount(Resume);
+
+function getMountedComponent(Component, propsData) {
+  return shallowMount(Component, {
+    propsData,
+  });
+}
+
+describe('Resume', () => {
+  it('render resume value', () => {
+    expect(getMountedComponent(Resume, {
+      value: 20.99,
+    }).text()).toBe('Total: R$ 20.99');
+  });
+});
